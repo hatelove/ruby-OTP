da794a8fda7dc1647c78174115ef0f12082119fa
Joey Chen <jay.hatelove@gmail.com>
2020-12-14 20:39:40 +0800

    refactor: refactor test cases by extract method

--- test/authentication_spec.rb
+++ test/authentication_spec.rb
@@ -1,39 +1,42 @@
 require 'rspec'
 require_relative '../lib/loads'
 
+def given_password(account, password)
+  allow(@profile).to receive(:password).with(account).and_return(password)
+end
+
+def given_otp(otp)
+  allow(@token).to receive(:random_token).and_return(otp)
+end
+
+def should_be_valid(account, password)
+  is_valid = @authentication.valid?(account, password)
+  expect(is_valid).to be(true)
+end
+
+def should_be_invalid(account, password)
+  is_valid = @authentication.valid?(account, password)
+  expect(is_valid).to be(false)
+end
+
 describe 'Authentication' do
   before do
-    # Do nothing
+    @profile = double
+    @token = double
+    @authentication = AuthenticationService.new(@profile, @token)
   end
 
-  after do
-    # Do nothing
-  end
   it 'should be valid' do
-    # profile = FakeProfile.new
-    profile = double
-    allow(profile).to receive(:password).with('joey').and_return('91')
-    # token = FakeToken.new
-    token = double
-    allow(token).to receive(:random_token).and_return('000000')
-    authentication = AuthenticationService.new(profile, token)
-    is_valid = authentication.valid?('joey', '91000000')
-    expect(is_valid).to be(true)
+    given_password('joey', '91')
+    given_otp('000000')
+    should_be_valid('joey', '91000000')
+  end
+
+  it 'should be invalid' do
+    given_password('joey', '91')
+    given_otp('000000')
+    should_be_invalid('joey', 'wrong password')
   end
 
 end
 
-# class FakeToken < RasTokenDao
-#   def random_token(account)
-#     return '000000'
-#   end
-# end
-#
-# class FakeProfile < ProfileDao
-#   def password(account)
-#     if account == 'joey'
-#       return '91'
-#     end
-#     return ''
-#   end
-# end
