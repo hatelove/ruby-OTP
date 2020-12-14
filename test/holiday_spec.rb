97038649c5a079e4e7f8e092ecc24084bdbebc06
Joey Chen <jay.hatelove@gmail.com>
2020-12-14 21:59:26 +0800

    holiday: pass test by mock Date.today

--- test/holiday_spec.rb
+++ test/holiday_spec.rb
@@ -1,15 +1,13 @@
 require 'rspec'
+require 'date'
 
 describe 'Holiday' do
   before do
     # Do nothing
   end
 
-  after do
-    # Do nothing
-  end
-
   it 'is xmas' do
+    allow(Date).to receive(:today).and_return(Date.new(2000, 12, 25))
     holiday = Holiday.new
     response = holiday.say_hello
     expect(response).to eq('Merry Xmas')
