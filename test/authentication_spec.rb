84b458d390bcb67e800e2663239463e63632b27e
Joey Chen <jay.hatelove@gmail.com>
2020-12-14 20:29:38 +0800

    refactor: use rspec double to stub profile and token

--- test/authentication_spec.rb
+++ test/authentication_spec.rb
@@ -10,8 +10,12 @@ describe 'Authentication' do
     # Do nothing
   end
   it 'should be valid' do
-    profile = FakeProfile.new
-    token = FakeToken.new
+    # profile = FakeProfile.new
+    profile = double
+    allow(profile).to receive(:password).with('joey').and_return('91')
+    # token = FakeToken.new
+    token = double
+    allow(token).to receive(:random_token).and_return('000000')
     authentication = AuthenticationService.new(profile, token)
     is_valid = authentication.valid?('joey', '91000000')
     expect(is_valid).to be(true)
@@ -19,17 +23,17 @@ describe 'Authentication' do
 
 end
 
-class FakeToken < RasTokenDao
-  def random_token(account)
-    return '000000'
-  end
-end
-
-class FakeProfile < ProfileDao
-  def password(account)
-    if account == 'joey'
-      return '91'
-    end
-    return ''
-  end
-end
+# class FakeToken < RasTokenDao
+#   def random_token(account)
+#     return '000000'
+#   end
+# end
+#
+# class FakeProfile < ProfileDao
+#   def password(account)
+#     if account == 'joey'
+#       return '91'
+#     end
+#     return ''
+#   end
+# end
